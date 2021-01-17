75b249ba595c3ee690cbe9bd8d970e8fb880d584
kapbka <karbka@gmail.com>
2021-01-17 11:53:23 +0000

    bot menu, test version

--- app/bot_telegram.py
+++ app/bot_telegram.py
@@ -27,9 +27,9 @@ class Menu:
             c.register(dispatcher)
 
     def display(self, bot, update):
-        bot.callback_query.message.edit_text(self.display_name, reply_markup=self.menu_keyboard())
+        bot.callback_query.message.edit_text(self.display_name, reply_markup=self.keyboard())
 
-    def menu_keyboard(self):
+    def keyboard(self):
         res = [[InlineKeyboardButton(c.display_name, callback_data=c.name)] for c in self.children]
         if self.parent:
             res.append([InlineKeyboardButton('Back to ' + self.parent.display_name, callback_data=self.parent.name)])
@@ -46,25 +46,33 @@ class Bot:
     def __init__(self, token):
         self.updater = Updater(token, use_context=True)
 
-        self.c3 = Menu('children3', [Menu('children4', [])])
+        m_filtered_slots = Menu('Monday', [])
+        m_all_available_slots = Menu('Monday', [])
 
-        login = LoginMenu('Authentication', [])
+        m_filter_slots = Menu('Filter slots', [m_filtered_slots])
+        m_show_all_available = Menu('Show all available slots', [m_all_available_slots])
 
-        c11 = Menu('children11', [login])
-        c12 = Menu('children12', [])
-        self.c3.parent = c11
+        self.filter = Menu('Filters', [m_filter_slots, m_show_all_available])
 
-        c1 = Menu('children1', [c11, c12])
-        c2 = Menu('children2', [])
-        self.root_menu = Menu('root', [c1, c2])
+        m_login = LoginMenu('Login', [self.filter])
+
+        m_book_slot = Menu('Book slot', [m_login])
+        m_book_slot_and_checkout = Menu('Book slot and checkout', [m_login])
+        self.filter.parent = m_book_slot
+
+        m_waitrose = Menu('Waitrose', [m_book_slot, m_book_slot_and_checkout])
+        m_tesco = Menu('Tesco', [])
+        # m_filter_slots.parent = m_waitrose
+        # m_show_all_available.parent = m_waitrose
+
+        self.root_menu = Menu('Main', [m_waitrose, m_tesco])
 
         self.updater.dispatcher.add_handler(CommandHandler('start', self.start_menu))
         self.root_menu.register(self.updater.dispatcher)
-
         self.updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.handle_text))
 
     def start_menu(self, bot, update):
-        bot.message.reply_text(self.root_menu.display_name, reply_markup=self.root_menu.menu_keyboard())
+        bot.message.reply_text(self.root_menu.display_name, reply_markup=self.root_menu.keyboard())
 
     def run(self):
         self.updater.start_polling()
@@ -80,7 +88,7 @@ class Bot:
             BotState.is_waiting_cvv = True
         elif BotState.is_waiting_cvv:
             BotState.is_waiting_cvv = False
-            update.message.reply_text(self.c3.display_name, reply_markup=self.c3.menu_keyboard())
+            update.message.reply_text(self.filter.display_name, reply_markup=self.filter.keyboard())
 
 
 if __name__ == '__main__':
